7cac00673529dd579057bc58af137c457946bdf7
Zach <zacharyabyrne@gmail.com>
2014-12-31 11:13:42 -0700

    Add queen attacks and test for queen attacks.

--- test_queen.py
+++ test_queen.py
@@ -20,3 +20,18 @@ class QueenTestSuite(TestCase):
         self.assertEquals(set(queen1.list_moves(pawns)),
                           set([(2, 2), (4, 4), (2, 4), (4, 2),
                                (3, 2), (3, 4), (2, 3), (4, 3)]))
+
+    def test_queen_attacks(self):
+        queen1 = Queen(Colour.WHITE, (3, 3))
+        pawns = [
+            Pawn(Colour.WHITE, (1, 1)),
+            Pawn(Colour.BLACK, (5, 5)),
+            Pawn(Colour.WHITE, (1, 5)),
+            Pawn(Colour.BLACK, (5, 1)),
+            Pawn(Colour.WHITE, (3, 1)),
+            Pawn(Colour.WHITE, (3, 5)),
+            Pawn(Colour.BLACK, (1, 3)),
+            Pawn(Colour.WHITE, (5, 3))
+            ]
+        self.assertEquals(set(queen1.list_attacks(pawns)),
+                          set([(5, 5), (5, 1), (1, 3)]))
