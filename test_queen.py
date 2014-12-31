9b3fe60d440eca8faee5bdd6810da4317c45b2fc
Zach <zacharyabyrne@gmail.com>
2014-12-31 11:08:57 -0700

    Add Queen class and test for queen moves.

---
+++ test_queen.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from queen import Queen
+from piece import Colour
+from pawn import Pawn
+
+class QueenTestSuite(TestCase):
+
+    def test_queen_moves(self):
+        queen1 = Queen(Colour.WHITE, (3, 3))
+        pawns = [
+            Pawn(Colour.WHITE, (1, 1)),
+            Pawn(Colour.WHITE, (5, 5)),
+            Pawn(Colour.WHITE, (1, 5)),
+            Pawn(Colour.WHITE, (5, 1)),
+            Pawn(Colour.WHITE, (3, 1)),
+            Pawn(Colour.WHITE, (3, 5)),
+            Pawn(Colour.WHITE, (1, 3)),
+            Pawn(Colour.WHITE, (5, 3))
+            ]
+        self.assertEquals(set(queen1.list_moves(pawns)),
+                          set([(2, 2), (4, 4), (2, 4), (4, 2),
+                               (3, 2), (3, 4), (2, 3), (4, 3)]))
