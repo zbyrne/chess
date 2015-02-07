6de18bf954dbc12964b1c893a59149db48384d5c
Zach Byrne <zacharyabyrne@gmail.com>
2015-02-07 14:26:56 -0700

    Add king attacks test.

--- test_king.py
+++ test_king.py
@@ -13,3 +13,10 @@ class KingTestSuite(TestCase):
         pawn4 = Pawn(Colour.WHITE, (5, 5))
         self.assertEquals(set(king1.list_moves([pawn1, pawn2, pawn3, pawn4])),
                           set([(3, 5), (5, 3), (5, 4), (3, 4)]))
+
+    def test_king_attacks(self):
+        king1 = King(Colour.WHITE, (4, 4))
+        pawn1 = Pawn(Colour.BLACK, (4, 5))
+        pawn2 = Pawn(Colour.BLACK, (4, 6))
+        self.assertEquals(set(king1.list_attacks([pawn1, pawn2])),
+                          set([(4, 5)]))
